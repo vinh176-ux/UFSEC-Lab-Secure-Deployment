6ae093e56cd2048c16f38e6241ee2e0183cac9b4
vinh176-ux <vinh176@gmail.com>
2026-08-13 17:28:24 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -6,18 +6,35 @@ const cors = require('cors');
 app.use(cors({ origin: "https://labdeploy-webapp-vinh.azurewebsites.net"}));
 
 // insecure: uses a default password if env var missing
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'admin123';
+const { DefaultAzureCredential } = require("@azure/identity");
+const { secretclient } = require("@azure/keyvault-secrets");
+
+const credentials = new DefaultAzureCredential();
+const vaultName = process.env.KEYVAULT_NAME;
+const url = 'https://${vaultName}.vault.azure.net';
+const client = new SecretClient(url, credential);
+
+async function getAdminPassword() {
+const secret = await cleint.getSecret("ADMIN-PASSWORD");
+return secret.value;
+}
 
 app.get('/admin', (req, res) => {
-  if (!process.env.ADMIN_PASSWORD) {
-    return res.status(500).send("Admin password missing - please configure ADMIN_PASSWORD");
-  }
-  const pw = req.query.pw;
-  if (pw === ADMIN_PASSWORD) {
-    res.send('Welcome admin');
-  } else {
-    res.status(401).send('Unauthorized');
-  }
+const auth = req.headers['authorization'];
+if (!auth || !auth.startWith("Basic ")) {
+res.setHeader("WWW-Authenticate", "Basic realm=admin");
+return res.status(401).send("Authentication required");
+}
+
+const base64= auth.split(" ")[1];
+const [user, pass] = Buffer.from(base64, "base64").toString().split(":");
+
+const ADMIN_PASSWORD = await getAdminPassword();
+if (user === "admin" && pass === ADMIN_PASSWORD) {
+res.send("Welcome admin");
+} else {
+res.status(401).send("unauthorized");
+}
 });
 
 // verbose error (debug) enabled in production
